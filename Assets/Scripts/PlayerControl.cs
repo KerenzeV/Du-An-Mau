ffb0faaa0913ddd9f67719c25f67630df0b99a6d
KerenzeV <khoatnvps41054@gmail.com>
2024-05-25 19:08:54 +0700

    code nhân vật và enemy

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,18 +1,66 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    public float speed;
+    Rigidbody2D rb;
+    public float jumpSpeed;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckDistance = 0.1f;
+    bool isTouchingGround;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        CheckGround();
+        Jump();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            spriteRenderer.flipX = true;
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            spriteRenderer.flipX = false;
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+
+    void CheckGround()
+    {
+        // Raycast xuống dưới để kiểm tra xem nhân vật có đang đứng trên mặt đất hay không
+        isTouchingGround = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isTouchingGround)
+        {
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            isTouchingGround = false;  
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Vẽ raycast trong editor để dễ dàng kiểm tra
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * groundCheckDistance);
     }
 }
