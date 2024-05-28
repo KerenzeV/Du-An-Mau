efe228775b43c776ab45e0897c74214cc4eac66c
KerenzeV <khoatnvps41054@gmail.com>
2024-05-28 15:38:56 +0700

    code

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -12,6 +12,8 @@ public class test : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckDistance = 0.1f;
     bool isTouchingGround;
+    bool isOnLadder; // Biến để xác định xem nhân vật có đang ở trên cầu thang không
+    float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -26,20 +28,20 @@ public class test : MonoBehaviour
         Move();
         CheckGround();
         Jump();
+        ClimbLadder(); 
     }
 
     void Move()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
+        // Di chuyển ngang
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        // Xoay hình ảnh khi di chuyển sang trái hoặc phải
+        if (horizontalInput < 0)
             spriteRenderer.flipX = true;
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
+        else if (horizontalInput > 0)
             spriteRenderer.flipX = false;
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
     }
 
     void CheckGround()
@@ -53,13 +55,50 @@ public class test : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isTouchingGround)
         {
             rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
-            isTouchingGround = false;  
+            isTouchingGround = false;
+        }
+    }
+
+    void ClimbLadder()
+    {
+        // Kiểm tra xem nhân vật có đang ở trên cầu thang không
+        if (isOnLadder)
+        {
+            // Lấy input dọc từ trục y (phím lên hoặc xuống)
+            verticalInput = Input.GetAxis("Vertical");
+
+            // Di chuyển nhân vật theo trục y dựa trên input dọc
+            rb.velocity = new Vector2(rb.velocity.x, verticalInput * speed);
+
+            // Không cho nhân vật nhảy khi đang trên cầu thang
+            if (Input.GetKeyDown(KeyCode.Space))
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kiểm tra khi nhân vật va chạm với cầu thang
+        if (other.CompareTag("Ladder"))
+        {
+            isOnLadder = true;
+            rb.gravityScale = 0; // Tắt trọng lực để nhân vật không rơi khi trên cầu thang
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Kiểm tra khi nhân vật rời khỏi cầu thang
+        if (other.CompareTag("Ladder"))
+        {
+            isOnLadder = false;
+            rb.gravityScale = 1; // Bật lại trọng lực khi nhân vật rời khỏi cầu thang
         }
     }
 
     void OnDrawGizmosSelected()
     {
-        // Vẽ raycast trong editor để dễ dàng kiểm tra
+        // Vẽ raycast trong editor để kiểm tra
         Gizmos.color = Color.red;
         Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * groundCheckDistance);
     }
